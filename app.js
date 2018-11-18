7f9879af478be85c8c4d74f643291abe3ae949c8
phuducnp <phuduc212@gmail.com>
2018-11-18 23:55:48 +0700

    084 Clearing Our Input Fields

--- app.js
+++ app.js
@@ -101,6 +101,20 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields =  document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -140,7 +154,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type); 
         // 4. Clear the fields
-
+        UICtrl.clearFields();
         // 5. Calculate and update budget
         
         // 6. Calculate and update percentages
