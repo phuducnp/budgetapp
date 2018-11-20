19c1a47a0d8ad4ed01965e778a0ebf27ba462ef8
phuducnp <phuduc212@gmail.com>
2018-11-20 22:57:06 +0700

    085 Updating the Budget Controller

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
+                value : parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
         
@@ -141,6 +141,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         
         
     };
+
+    var updateBudget = function(){
+        // 1. Calculate and update budget
+
+        // 2. Return the budget
+        
+        // 6. Display the budget on the UI
+    };
     
     var ctrlAddItem = function() {
         var input, newItem;
@@ -148,6 +156,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getInput();        
         
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
         // 2. Add the item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -155,9 +164,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         UICtrl.addListItem(newItem, input.type); 
         // 4. Clear the fields
         UICtrl.clearFields();
-        // 5. Calculate and update budget
         
-        // 6. Calculate and update percentages
+        // Calculate and update budget
+        updateBudget();
+        }
     };
     
     return {
