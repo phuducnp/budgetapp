0be1b86c928145c0f9b70f1a6f4d205dd8c728cf
phuducnp <phuduc212@gmail.com>
2018-11-21 22:35:43 +0700

    086 Updating the Budget Budget Controller

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
         this.value = value;
     };
     
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function(cur){
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -22,6 +30,8 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         },
+        budget: 0,
+        percentage: -1
     };
     
     return {
@@ -51,6 +61,29 @@ var budgetController = (function() {
             
             // Return the new element
             return newItem;
+        },
+
+        calculateBudget: function(){
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            // calculate the percentage of income that we spent
+            if(data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function(){
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
         },
         
         testing: function() {
@@ -144,10 +177,11 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var updateBudget = function(){
         // 1. Calculate and update budget
-
+        budgetCtrl.calculateBudget();
         // 2. Return the budget
-        
-        // 6. Display the budget on the UI
+        var budget = budgetCtrl.getBudget();
+        // 3. Display the budget on the UI
+        console.log(budget);
     };
     
     var ctrlAddItem = function() {
